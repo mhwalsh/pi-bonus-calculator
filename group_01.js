d550874db8b0a03ffb1a7cc29c9aecf167a89c76
Millie Walsh <millie@primeacademy.io>
2016-08-26 11:58:16 -0500

    add calc function and called it

--- group_01.js
+++ group_01.js
@@ -1,3 +1,6 @@
+// -- global data -- //
+
+// employee array = [name, employee number, annual salary, review rating]
 var atticus = ["Atticus", "2405", "47000", 3];
 var jem = ["Jem", "62347", "63500", 4];
 var boo = ["Boo", "11435", "54000", 3];
@@ -5,6 +8,25 @@ var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
 
+// function calcBonus takes an employee and
+// calculates their bonus and returns a new employee array.
+function calcBonus(anEmployee) {
+
+  // marshal variables
+  var empName = anEmployee[0];
+  var empNumber = anEmployee[1];
+  var empSalary = anEmployee [2];
+  var empRating = anEmployee[3];
+
+  console.log('empName =', empName);
+  console.log('empNumber =', empNumber);
+  console.log('empSalary =', empSalary);
+  console.log('empRating =', empRating);
+
+  //
+}
+
+// -- Main Execution -- //
 for (var i = 0; i < employees.length; i++) {
-  console.log(employees[i][0]);
+  calcBonus(employees[i]);
 }
